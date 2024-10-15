e362bcfc0379e2e671f9a53e1693ce3254be37c1
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2024-10-15 23:24:50 +0100

    Use _commit to force flush

--- asApp/src/save_restore.c
+++ asApp/src/save_restore.c
@@ -140,6 +140,7 @@
 #else
 #include    <windows.h>
 #include	"tr_dirent.h" /* for dirList */
+#include    <io.h> /* for _commit */
 #endif
 #include	<string.h>
 #include	<ctype.h>
@@ -1936,7 +1937,10 @@ STATIC int write_it(char *filename, struct chlist *plist)
 		if (errno) myPrintErrno("write_it", __FILE__, __LINE__);
 	}
 #elif defined(_WIN32)
-        /* WIN32 has no real equivalent to fsync? */
+    n = _commit(fileno(out_fd));
+    if (n) {
+		printf("save_restore:write_it: _commit failed [%s]\n", n, datetime);
+    }
 #else
 	n = fsync(fileno(out_fd));
 	if (n && (errno == ENOTSUP)) { n = 0; errno = 0; }
